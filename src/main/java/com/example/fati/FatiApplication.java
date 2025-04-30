613449e4ee02289ff5a3afeab8cde607449c0473
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-05-01 02:18:53 +0300

    Done !

--- src/main/java/com/example/fati/FatiApplication.java
+++ src/main/java/com/example/fati/FatiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FatiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FatiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FatiApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/fati/FatiApplicationTests.java
+++ src/test/java/com/example/fati/FatiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FatiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
